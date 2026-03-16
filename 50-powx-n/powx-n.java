c1c836dcd2eb449b5d4ed3c1df88242bf459c475
Aditya Kumar Pansary <adipsy2175@gmail.com>
2026-03-17 01:25:36 +0530

    Time: 0 ms (100.00%) | Memory: 47.8 MB (77.62%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -2,5 +2,14 @@ class Solution {
     public double myPow(double x, int n) {
         double power = Math.pow(x,n);
         return power;
+
+        // // base case 
+        // if(n==0){
+        //     return 1;
+        // }
+        // //kaam
+        // int fnm1 = myPow(x,n-1);
+        // int fn = x*fnm1;
+        // return fn;
     }
-}
+}  
